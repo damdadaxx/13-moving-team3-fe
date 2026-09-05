6b96210b6eae191eb079287c7c8634b9f53d77d6
damdadaxx <hyojudev@gmail.com>
2026-09-05 18:40:07 +0900

    chore: 공통 라이브러리와 커밋/포맷 설정 추가

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'style', 'test', 'docs', 'chore', 'refactor'],
+    ],
     'subject-case': [0],
   },
 };

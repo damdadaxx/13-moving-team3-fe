24c65abc3cdd8081bf9791d2489d835a0849b197
bangminjae <abab1070@naver.com>
2026-09-14 11:49:53 +0900

    fix: buttonStyle 수정

--- src/components/ui/Button/ButtonStyles.ts
+++ src/components/ui/Button/ButtonStyles.ts
@@ -24,12 +24,13 @@ export const buttonVariants = cva(BUTTON_BASE_CLASS, {
       solid:
         'bg-orange-400 p-4 text-gray-50 not-aria-disabled:hover:bg-orange-500 aria-disabled:bg-gray-300',
       outlined:
-        'border border-orange-400 px-6 py-4 text-orange-400 shadow-[4px_4px_10px_0_rgba(195,217,242,0.2)] not-aria-disabled:hover:bg-orange-100 not-aria-disabled:hover:shadow-[4px_4px_5px_0_rgba(195,217,242,0.2)] aria-disabled:border-[#c4c4c4] aria-disabled:text-[#808080]',
+        'border border-orange-400 px-6 py-4 text-orange-400 shadow-[4px_4px_10px_0_rgba(195,217,242,0.2)] not-aria-disabled:hover:bg-orange-100 not-aria-disabled:hover:shadow-[4px_4px_5px_0_rgba(195,217,242,0.2)] aria-disabled:border-gray-200 aria-disabled:text-gray-500',
     },
     size: {
-      sm: 'h-[54px] gap-1 rounded-[12px] text-lg-semibold',
-      md: 'h-[60px] gap-2 rounded-[16px] text-2lg-semibold',
-      lg: 'h-[64px] gap-2 rounded-[16px] text-2lg-semibold',
+      xs: 'h-[54px] gap-1 rounded-[12px] text-lg-semibold',
+      sm: 'h-[54px] gap-1 rounded-[12px] text-lg-semibold tablet:text-2lg-semibold tablet:rounded-[16px] pc:h-[60px]',
+      md: 'h-[60px] gap-2 rounded-[12px] text-2lg-semibold pc:h-[64px] pc:rounded-[12px] pc:text-2lg-semibold',
+      lg: 'h-[64px] gap-2 rounded-[12px] text-2lg-semibold tablet:h-[64px] pc:rounded-[12px] pc:text-2lg-semibold',
     },
   },
   defaultVariants: {
